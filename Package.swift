21689f0860bb72359034b6b57741caf6a4e97b8c
ThePotatoKing55 <thepotatoking55@gmail.com>
2021-07-31 13:15:10 -0500

    build as dylib

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftGLFW",
     products: [
-        .library(name: "GLFW", targets: ["GLFW"])
+        .library(name: "GLFW", type: .dynamic, targets: ["GLFW"])
     ],
     dependencies: [
-        .package(url: "https://github.com/thepotatoking55/CGLFW3", .branch("main"))
+        .package(url: "https://github.com/thepotatoking55/CGLFW3.git", .branch("main"))
     ],
     targets: [
         .target(name: "GLFW", dependencies: ["CGLFW3"])
