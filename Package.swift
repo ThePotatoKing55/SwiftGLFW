a3043e268c7c4370c4321f06daab9bc582291269
ThePotatoKing55 <thepotatoking55@gmail.com>
2021-08-01 18:46:34 -0500

    add CGLFW3 as product library

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftGLFW",
     products: [
-        .library(name: "GLFW", targets: ["GLFW"])
+        .library(name: "GLFW", targets: ["GLFW"]),
+        .library(name: "CGLFW", targets: ["CGLFW3"])
     ],
     targets: [
         .systemLibrary(name: "CGLFW3", pkgConfig: "glfw3", providers: [.brew(["glfw3"])]),
