0c2b04f8f072af41583bb1b9681a2845eeae0f31
Adora Cline <thepotatoking55@gmail.com>
2022-04-06 11:12:36 -0500

    add Image initializer

--- Sources/GLFW/Structs/Image.swift
+++ Sources/GLFW/Structs/Image.swift
@@ -4,6 +4,12 @@ public struct Image: Hashable, Sendable {
     public var pixels: [Color]
     public var width, height: Int
     
+    public init(width: Int, height: Int, pixels: [Color] = []) {
+        self.pixels = pixels
+        self.width = width
+        self.height = height
+    }
+    
     var glfwImage: GLFWimage {
         pixels.withUnsafeBytes { bytes in
             let ptr = UnsafeMutablePointer(mutating: bytes.baseAddress!.assumingMemoryBound(to: UInt8.self))
